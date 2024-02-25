d80a5add113e4b89ac323eb27d915592ed148749
lamissb <107209872+lamissb@users.noreply.github.com>
2024-02-25 20:31:56 +0100

    Update TheComponent.js

--- src/components/TheComponent.js
+++ src/components/TheComponent.js
@@ -24,9 +24,23 @@ addDoc(collection(db,"collectionName"),{=> YOUR DATA <=}) with random doc ID (NE
 
 export function TheComponent(){
   const [data,setData] = useState({}); //the page document : webdev object for example
+  const [user,setUser] = useState({});
   const page=useParams().id
-    
+  
   useEffect(()=>{
+    const getUser = async () =>{
+      try{
+        const data = await getDocs(collection(db,"users"));
+        const filteredData = data.docs.map((doc)=>({
+          ...doc.data(),
+          id:doc.id
+        }));
+        setUser(filteredData.find((user)=>user.uid==auth?.currentUser?.uid));
+      }
+      catch(err){
+        console.error(err);
+      }
+    }
     async function getData(){
       try{
         const dataRef = (await getDoc(doc(db,"pages",page))).data();
@@ -36,37 +50,42 @@ export function TheComponent(){
       }
     }
     getData();
+    getUser()
 
   },[])
 
   return(
     <main className="page">
       {/*<button onClick={()=>{updateDoc(doc(collection(db,"pages"),"mobile-development"),{steps:steps})}}>hello</button>*/}
-      {/*<button onClick={()=>console.log(getPersonalPath())}>hello</button>*/}
+      {/*<button onClick={()=>console.log(user?.personalPaths[page])}>hello</button>*/}
       <div className='jumbotron text-center'>
         <h1 className='display-4'>
           Find the best path for {data.subject} 
         </h1>  
       </div>
-      {auth?.currentUser && 
-        <div>take the quizz</div>
+      {!auth?.currentUser &&
+        <div>Sign up to take the personality test and get your own personal path!</div>
       }
-      {auth.currentUser && 
+      {auth?.currentUser && !user.personalPaths &&
+        <div>take the personality test</div>
+      }
+      {auth.currentUser && user?.personalPaths &&
       <div className='personal-path d-flex justify-content-sm-center'>
         <h3>
           Based on your personality and personal preferences, we found that the best free way for you to learn  {data.subject} is this way, HAPPY LEARNING!
         </h3>
         <div className='path'>
+          <PersonalPath personalPath={user?.personalPaths[page]}/>
         </div>
       </div>
       }
         
-          <div className='d-flex justify-content-sm-center align-self-center'>
-              <h5>What people think</h5>
-          </div>
           <div className='row justify-content-md-between'>
             <div className='col-md-3 ml-md-auto public'>
-              <PublicPath publicPaths={data.publicPaths}/>
+              <div className='d-flex justify-content-sm-center align-self-center'>
+                  <h5>What people think</h5>
+              </div>
+              <PublicPath publicPaths={data?.publicPaths} isActive={auth?.currentUser ? true : false}/>
             </div>
             <div className='col-md-3 ml-md-auto create'>
               {auth.currentUser && 
